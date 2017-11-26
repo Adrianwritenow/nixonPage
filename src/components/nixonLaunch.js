823b1ea55b33182929b5dd51fcd634120cc5fadc
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-11-25 20:48:51 -0500

    added styling and social media buttons

--- src/components/nixonLaunch.js
+++ src/components/nixonLaunch.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+
 class Launch extends Component {
+
   render() {
+    let details = {userFirstName:'Nacho', watchStyle:'51-30'};
+
     return (
       <div className="launchWrapper">
-        <div className="header"><h1>NIXONLAUNCH</h1></div>
+        <div className="header"><h1>Time Tailored To You</h1></div>
+        <section className="animation-box">
+        <div className="welcomeMsg">Welcome to the NIXON family {details.userFirstName}</div>
+        </section>
+        <img src="https://theawesomer.com/photos/2008/12/122208_nixon_1.jpg" alt='Your Watch '/>
+        <div className="watchLine">Your custom {details.watchStyle} looks great</div>
+        <div className="mediaLine"> Now Take Some Time To Share Your Machine<br/>
+          <div className="social__container">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-pinterest"></a>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/splash.js
+++ src/components/splash.js
@@ -9,7 +9,6 @@ class Splash extends Component {
       <div className="SplashWrapper">
         <div className="header"><h1>Splash</h1></div>
         <NavLink activeClassName="selected" className="nav-link" exact to="/nixonLaunch" ><button className="button" type="submit">Continue</button></NavLink>
-
       </div>
     );
   }
